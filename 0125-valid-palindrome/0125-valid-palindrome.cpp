e54a46fb8be5c80664f051873e70ea7d65d0ce18
Arpit8789 <147298883+Arpit8789@users.noreply.github.com>
2025-03-03 22:20:42 +0530

    Time: 0 ms (100%), Space: 9.9 MB (57.48%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,60 +1,21 @@
-#include<string>
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <cctype>
 using namespace std;
-class Solution {
-private:
-    bool valid(char ch) {
-        if( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
-            return 1;
-        }
-        
-        return 0;
-    }
-    
-    char toLowerCase(char ch) {
-    if( (ch >='a' && ch <='z') || (ch >='0' && ch <='9') )
-        return ch;
-    else{
-        char temp = ch - 'A' + 'a';
-        return temp;
-    }
-}
-    bool checkPalindrome(string a) {
-    int s = 0;
-    int e = a.length()-1;
 
-    while(s<=e) {
-        if(a[s] != a[e])
-        {
-            return 0;       
-        }
-        else{
-            s++;
-            e--;
-        }
-    }
-    return 1;
-}
+class Solution {
 public:
     bool isPalindrome(string s) {
-        
-        //faltu character hatado
-        string temp = "";
-        
-        for(int j=0; j<s.length(); j++) {   
-            if(valid(s[j])) {
-                temp.push_back(s[j]);
-            }
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            while (left < right && !isalnum(s[left])) left++;   // Skip non-alphanumeric
+            while (left < right && !isalnum(s[right])) right--; // Skip non-alphanumeric
+            
+            if (tolower(s[left]) != tolower(s[right])) return false;
+            
+            left++;
+            right--;
         }
-        
-        //lowercase me kardo
-        for(int j=0; j<temp.length(); j++) { 
-            temp[j] = toLowerCase(temp[j]);
-        }
-        
-        //check palindrome
-        return checkPalindrome(temp);
-        
+        return true;
     }
 };
