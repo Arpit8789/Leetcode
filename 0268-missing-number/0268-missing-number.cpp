9f011abbc144e39380ef36becf3bb90d0f2d204f
Arpit Anand <Arpitanand2611@gmail.com>
2025-08-14 23:05:36 +0530

    Time: 13 ms (11.3%), Space: 27.8 MB (5.03%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,20 +2,25 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        vector<int> arr(n + 1, 0);  // Initialize a vector of size n+1 with all zeros
-        
-        // Mark present numbers
-        for (int num : nums) {
-            arr[num] = 1;
-        }
-        
-        // Find the missing number
+
+        unordered_map<int, int> mpp;
+
         for (int i = 0; i <= n; i++) {
-            if (arr[i] == 0) {
-                return i;
+            mpp[i] = 0;
+        }
+
+        
+        for (int num : nums) {
+            mpp[num] = 1; 
+        }
+
+        
+        for (auto it : mpp) {
+            if (it.second == 0) {
+                return it.first;
             }
         }
-        
-        return -1;  // This line should never be reached if the input is valid
+
+        return -1;
     }
 };
