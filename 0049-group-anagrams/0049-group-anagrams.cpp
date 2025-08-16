4c3686b88409eaf44a8c59e9107ee4a612347d89
Arpit Anand <Arpitanand2611@gmail.com>
2025-08-16 21:40:17 +0530

    Time: 15 ms (71.78%), Space: 26.1 MB (47.97%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,41 +1,18 @@
-
-
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        int n = strs.size();
-        vector<pair<string, string>> sortedWords;
+        unordered_map<string, vector<string>> mpp;
 
-        // Step 1: Store each word along with its sorted form
-        for (const string& word : strs) {
-            string sortedWord = word;
-            sort(sortedWord.begin(), sortedWord.end());  // Sort to identify anagrams
-            sortedWords.push_back({sortedWord, word});
+        for (string s : strs) {
+            string key = s;                 // copy original
+            sort(key.begin(), key.end());   // sort to form key
+            mpp[key].push_back(s);          // group anagrams
         }
 
-        // Step 2: Sort the vector of pairs based on the sorted word
-        sort(sortedWords.begin(), sortedWords.end());
-
-        // Step 3: Group anagrams manually
-        vector<vector<string>> result;
-        vector<string> currentGroup;
-
-        for (int i = 0; i < n; i++) {
-            if (i == 0 || sortedWords[i].first == sortedWords[i - 1].first) {
-                // Add to the current anagram group
-                currentGroup.push_back(sortedWords[i].second);
-            } else {
-                // Store previous group and start a new one
-                result.push_back(currentGroup);
-                currentGroup = {sortedWords[i].second};
-            }
+        vector<vector<string>> ans;
+        for (auto it : mpp) {
+            ans.push_back(it.second);
         }
-
-        // Add the last group
-        if (!currentGroup.empty()) {
-            result.push_back(currentGroup);
-        }
-
-        return result;
+        return ans;
     }
 };
