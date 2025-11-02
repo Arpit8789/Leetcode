12db973a28c716b69464d09fffe5439aefe9ea24
Arpit Anand <Arpitanand2611@gmail.com>
2025-11-02 22:18:51 +0530

    Time: 10 ms (46.02%), Space: 7.6 MB (86.93%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
-    int helper(int n)
-    {
+    int fib(int n) {
         if(n==1)
             return 1;
-        else if(n==0)
+        if(n==0)
             return 0;
-        return helper(n-1)+helper(n-2);
-    }
-    int fib(int n) {
-        return helper(n);
+        
+        return fib(n-1)+fib(n-2);
+        
     }
 };
