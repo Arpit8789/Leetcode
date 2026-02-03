d82e7a8009b2af3977c3454ed638ec034342f18f
Arpit Anand <Arpitanand2611@gmail.com>
2026-02-03 19:04:00 +0530

    Time: 13 ms (50.17%), Space: 19.4 MB (5.1%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,29 +1,58 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
+    bool gotP = false;
+    bool gotQ = false;
+
 public:
+    void solve(TreeNode* root, TreeNode* p, TreeNode* q,
+               vector<vector<TreeNode*>>& ans,
+               vector<TreeNode*>& v)
+    {
+        if (root == NULL)
+            return;
+
+        v.push_back(root);
+
+        if (root == p && !gotP) {
+            ans.push_back(v);
+            gotP = true;
+        }
+
+        if (root == q && !gotQ) {
+            ans.push_back(v);
+            gotQ = true;
+        }
+
+        solve(root->left, p, q, ans, v);
+        solve(root->right, p, q, ans, v);
+
+        v.pop_back();
+    }
+
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==NULL)
+        if (root == NULL)
             return NULL;
 
-        if(root==p || root==q)
-            return root;
-        TreeNode* leftans=lowestCommonAncestor(root->left,p,q);
-        TreeNode* rightans=lowestCommonAncestor(root->right,p,q);
-        if(leftans!=NULL && rightans!=NULL)
-            return root;
-        else if(leftans!=NULL && rightans==NULL)
-            return leftans;
-        else if(rightans!=NULL && leftans==NULL)
-            return rightans;
-        return NULL;
+        vector<vector<TreeNode*>> ans;
+        vector<TreeNode*> v;
 
+        solve(root, p, q, ans, v);
+
+        if (ans.size() == 1)
+            return ans[0].back();   
+
+        if (ans.size() < 2)
+            return NULL;
+
+        TreeNode* lca = NULL;
+        int sz = min(ans[0].size(), ans[1].size());
+
+        for (int i = 0; i < sz; i++) {
+            if (ans[0][i] == ans[1][i])
+                lca = ans[0][i];
+            else
+                break;
+        }
+
+        return lca;
     }
 };
