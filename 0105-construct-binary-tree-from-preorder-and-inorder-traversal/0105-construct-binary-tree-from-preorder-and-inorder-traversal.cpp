def2ef27f947c48ea36d1cb4d1d1f086330e09f5
Arpit Anand <Arpitanand2611@gmail.com>
2026-02-14 13:59:38 +0530

    Time: 0 ms (100%), Space: 27.5 MB (53.21%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,23 +1,15 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-    TreeNode* solve(vector<int>& pre, vector<int>& in,auto& mpp,int s,int e,int& beg){
-        if(s>e) return 0;
-        int idx=mpp[pre[beg]];
-        // cout<<pre[beg]<<endl;
-        TreeNode* root=new TreeNode(pre[beg++]);
-        root->left=solve(pre,in,mpp,s,idx-1,beg);
-        root->right=solve(pre,in,mpp,idx+1,e,beg);
+    TreeNode* solve(vector<int>& preorder, vector<int>& inorder, map<int,int>& mpp,int s,int e,int& i)
+    {
+        if(s>e)
+            return 0;
+        int rootindx = mpp[preorder[i]];
+        TreeNode* root = new TreeNode(preorder[i]);
+        i++;
+        root->left=solve(preorder,inorder,mpp,s,rootindx-1,i);
+        root->right=solve(preorder,inorder,mpp,rootindx+1,e,i);
         return root;
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
@@ -26,7 +18,8 @@ public:
         {
             mpp[inorder[i]]=i;
         }
-        int beg=0;
-        return solve(preorder,inorder,mpp,0,inorder.size()-1,beg);
+        int i=0;
+        TreeNode* root=solve(preorder,inorder,mpp,0,inorder.size()-1,i);
+        return root;
     }
 };
